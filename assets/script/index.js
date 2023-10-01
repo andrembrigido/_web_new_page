d2c0b17aa1d529ba10e1358fe10f72bdbfcb157e
text <text>
2023-09-30 23:50:38 -0400

    Update

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,16 @@
-alert("hello Word");
+/*-----------------------------------------------------*/
+/*----------------OPEN AND CLOSE MENU------------------*/
+/*-----------------------------------------------------*/
+
+let btnMenuVar = document.getElementById("btnMenu");
+
+function toggleMenu() {
+  let navMenuVar = document.getElementById("navMenu");
+  navMenuVar.classList.toggle("active"); //toggle adiciona a classe caso tenha e remove caso nao tenha
+}
+
+btnMenuVar.addEventListener("click", toggleMenu);
+
+/*-----------------------------------------------------*/
+/*-----------------------------------------------------*/
+/*-----------------------------------------------------*/
